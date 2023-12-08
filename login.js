798cef9b9460f893e2bcb15d7761f77214410c16
rayveneja <rayvenejenningsallen@gmail.com>
2023-12-08 15:11:48 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,49 @@
 function attemptLogin() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    var emailInput = document.getElementById('email');
+    var passwordInput = document.getElementById('password');
+    var loginMessage = document.getElementById('loginMessage');
 
-    // Simple validation for demonstration
-    if (email === "user123@gmail.com" && password === "password") {
-        loginMessage.style.color = "#28a745";
-        loginMessage.textContent = "Login successful! Loading the Dashboard...";
-
-        // Save authentication status (you may use more secure methods in a real application)
-        localStorage.setItem('authenticated', 'true');
-
-        // Redirect to the dashboard after a short delay
-        setTimeout(() => {
-            window.location.href = 'dashboard.html';
-        }, 1500);
-    } else {
-        loginMessage.style.color = "#dc3545";
-        loginMessage.textContent = "Invalid email address or password. Please try again.";
+    if (!isValidEmail(emailInput.value)) {
+        loginMessage.innerHTML = "Invalid email address.";
+        return;
     }
+
+    var email = sanitizeInput(emailInput.value);
+    var password = sanitizeInput(passwordInput.value);
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                var response = JSON.parse(xhr.responseText);
+                handleLoginResponse(response);
+            } else {
+                loginMessage.innerHTML = "Server error. Please try again. Status: " + xhr.status + ", Response: " + xhr.responseText;
+
+            }
+        }
+    };
+
+    xhr.open("POST", "login.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("email=" + email + "&password=" + password);
+}
+
+function handleLoginResponse(response) {
+    var loginMessage = document.getElementById('loginMessage');
+
+    if (response.success) {
+        window.location.href = 'dashboard.html';
+    } else {
+        loginMessage.innerHTML = "Login failed. " + response.error;
+    }
+}
+
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function sanitizeInput(input) {
+    return input.trim();
 }
